24224ff9e5651a788f8d90bafe56692a642c5fa0
Matheus Wisch <wisch.pg@outlook.com>
2025-07-30 15:20:05 -0300

    feat: create project and test

---
+++ MoneyExtension.Tests/MoneyExtensionTests.cs
@@ -0,0 +1,14 @@
+﻿namespace MoneyExtension.Tests;
+
+[TestClass]
+public sealed class MoneyExtensionTests
+{
+    [TestMethod]
+    public void ShouldConvertDecimalToInt()
+    {
+        const decimal value = 279.98M;
+        var cents = value.ToCents();
+        
+        Assert.AreEqual(27998, cents);
+    }
+}
